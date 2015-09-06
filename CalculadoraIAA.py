4c41f0ef73a65b585b05bf27e2475d5cebe1bdcc
Gustavo Zambonin <stenny_gus@hotmail.com>
2015-09-06 15:40:58 -0300

    calculates the final index correctly * shebang added for practicity * extra import removed * all function names standardized * code width normalized to 80 characters * functions mostly in english now * not equal operator substituted (former is deprecated) * semi-colons removed * string manipulation updated with intended operators * html special characters are encoded to utf-8 properly * less use of line breaks to compact script output * prevents the user from inputting invalid grades * general refactoring

--- CalculadoraIAA.py
+++ CalculadoraIAA.py
@@ -1,8 +1,8 @@
-#coding: utf-8
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
 
 import mechanize
 import getpass
-import os
 
 from HTMLParser import HTMLParser
 
@@ -10,8 +10,10 @@ class MLStripper(HTMLParser):
     def __init__(self):
         self.reset()
         self.fed = []
+
     def handle_data(self, d):
         self.fed.append(d)
+
     def get_data(self):
         return ''.join(self.fed)
 
@@ -20,15 +22,20 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
 
-def getHistory(br):
-    return br.open("https://cagr.sistemas.ufsc.br/modules/aluno/historicoEscolar/")
+def get_history(br):
+    hist = "https://cagr.sistemas.ufsc.br/modules/aluno/historicoEscolar/"
+    return br.open(hist)
 
-def getMirror(br):
-    return br.open("https://cagr.sistemas.ufsc.br/modules/aluno/espelhoMatricula/")
+def get_mirror(br):
+    mirror = "https://cagr.sistemas.ufsc.br/modules/aluno/espelhoMatricula/"
+    return br.open(mirror)
 
 def conectar_CAGR(username, password):
     br = mechanize.Browser()
-    response = br.open("https://sistemas.ufsc.br/login?service=https%3A%2F%2Fcagr.sistemas.ufsc.br%2Fj_spring_cas_security_check&userType=padrao&convertToUserType=alunoGraduacao&lockUserType=1")
+    cagr_link = ("https://sistemas.ufsc.br/login?service=https%3A%2F%2F"
+                "cagr.sistemas.ufsc.br%2Fj_spring_cas_security_check&userType="
+                "padrao&convertToUserType=alunoGraduacao&lockUserType=1")
+    response = br.open(cagr_link)
 
     for form1 in br.forms():
         form = form1
@@ -42,94 +49,89 @@ def conectar_CAGR(username, password):
 
     return br
 
-
 def get_IAA(text, size):
-    for i in range(0,2):
+    for i in range(2):
         size = text.rfind("IAA", 0, size)
-
-        inicio = size
-        fim = text.find("IAP", inicio, len(text))
-    text = strip_tags(text [inicio + 4:fim])
+        start = size
+        end = text.find("IAP", start, len(text))
+    text = strip_tags(text [start + 4:end])
     text = [t.replace(',', '.') for t in text]
     IAA = text[1:len(text)-1]
     return float("".join(IAA))
 
 def get_cargaHoraria(text):
     size = len(text)
-    inicio = text.rfind("total:", 0, size)
-    fim = text.find(" a", inicio, size)
-    text = text [inicio + 7 : fim - 2]
+    start = text.rfind("total:", 0, size)
+    end = text.find(" a", start, size)
+    text = text[start+7:end-2]
     return int(text)
 
 def get_nome(text):
     size = len(text)
-    inicio = text.find("rich-panel-header", 0, size)
-    inicio = text.find(">", inicio, size)
-    fim = text.find("<", inicio, size)
-    text = text [inicio + 1:fim]
+    start = text.find("rich-panel-header", 0, size)
+    start = text.find(">", start, size)
+    end = text.find("<", start, size)
+    text = text[start+1:end]
     return text
 
-def getLgth(text):
-    ret = 0; 
+def get_length(text):
+    ret = 0
     size = len(text)
     types = [">Ob<",">Ex<",">Op<"]
     for s in types:    
         i = 0
-        while i <> -1:
+        while i != -1:
             i = text.find(s, i+1, size)
-	    if i <> -1:
-	        ret = ret+1
-   
-    return ret-1;
+	    if i != -1:
+	        ret += 1
+    return ret-1
 
 def dName(text, pos):
     size = len(text)
-    strr = "{}:j_id213\">".format(pos)
-    init = text.rfind(strr, 0, size)
-    fim = text.find("</td>", init, size)
-    text = text [init + len(strr):fim]
-    return text
+    strr = "%s:j_id213\">" % pos
+    start = text.rfind(strr, 0, size)
+    end = text.find("</td>", start, size)
+    text = text[start + len(strr):end]
+    fixed_text = HTMLParser().unescape(text)
+    return fixed_text.encode('utf-8', 'ignore')
 
 def dHA(text, pos):
     size = len(text)
-    strr = "{}:j_id219\" style=\"text-align: center\">".format(pos)
-    init = text.rfind(strr, 0, size)
-    fim = text.find("</td>", init, size)
-    text = text [init + len(strr):fim]
+    strr = "%s:j_id219\" style=\"text-align: center\">" % pos
+    start = text.rfind(strr, 0, size)
+    end = text.find("</td>", start, size)
+    text = text[start + len(strr):end]
     return int(text)
 
-#PROGRAMA COMECA AQUI
-
-username = raw_input("Insira sua matrícula:\n")
-password = getpass.getpass("\nInsira sua senha do CAGR:\n")
+username = raw_input("Insira sua matrícula: ")
+password = getpass.getpass("Insira sua senha do CAGR: ")
 
 response = conectar_CAGR(username, password);
-
-history = getHistory(response)
+history = get_history(response)
 
 pagina = history.read()
 
-IAA = get_IAA(pagina,len(pagina))
+IAA = get_IAA(pagina, len(pagina))
 cargaHoraria = get_cargaHoraria(pagina)
 nome = get_nome(pagina)
 
-print("Olá {} !".format(nome))
-print("Atualmente seu IAA é {} e sua carga horária total cursada é de {} horas".format(str(IAA), str(cargaHoraria)))
+print("Olá, %s!" % nome)
+print("Seu IAA é %.2f e sua carga horária cursada é de %dh." % (IAA, cargaHoraria))
 
-
-mirror = getMirror(response)
-
+mirror = get_mirror(response)
 pagina = mirror.read()
-totalAulas = getLgth(pagina)
+totalAulas = get_length(pagina)
 
-horasAulasTotal = 0
-somatorio = 0
-for i in range(0,totalAulas):
-    horasAulas = dHA(pagina, i)
+horasAulasTotal, somatorio = 0, 0
+for i in range(totalAulas):
+    horasAulas = dHA(pagina, i)*18
     horasAulasTotal += horasAulas
-    notaAula = float(raw_input("Insira a possivel nota em {}\n".format(dName(pagina, i))))
+    notaAula = float(raw_input("Insira a possível nota em %s: " % dName(pagina, i)))
+    while notaAula > 10 or notaAula < 0:
+        notaAula = float(raw_input("Nota inválida. Possível nota: "))
     somatorio += notaAula*horasAulas
 horasAulasTotal += cargaHoraria
 somatorio += IAA*cargaHoraria
+IAAFinal = somatorio/horasAulasTotal
 
-print ("De acordo com o que você me informou, seu possível IAA será {}".format(str("{0:.2f}".format(somatorio/horasAulasTotal))))
+print ("Com as notas informadas, seu possível IAA será %.2f." % IAAFinal)
---
+++ README.md
@@ -0,0 +1,12 @@
+# CalculadoraIAA
+
+Uma simples calculadora para previsão de IAA ao longo do semestre. 
+
+Python 2 é necessário por conta do módulo `mechanize`, que pode ser instalado com a ferramenta `pip` ou através de pacotes da sua distribuição preferida.
+
+Métodos de utilização:
+```
+$ .\CalculadoraIAA.py
+
+$ python2 CalculadoraIAA.py
+```
